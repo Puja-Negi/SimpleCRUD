241bb6c0c294c8a399b417610498a901958dadd1
Puja-Negi <Puja.negi@truenary.com>
2023-12-20 12:21:34 +0545

    Simple MVC Category CRUD

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -8,7 +8,6 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        //validation
         [MaxLength(30)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
