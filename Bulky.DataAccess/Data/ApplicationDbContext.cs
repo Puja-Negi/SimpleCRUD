1fb1b8e8afc57ae7ab207dfe2b8a2fb0b1c10d7f
Puja-Negi <Puja.negi@truenary.com>
2023-12-28 12:19:59 +0545

    refactor: implemenatation of CRUD for Product

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ namespace Bulky.DataAccess.Data
 
         }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>().HasData(
@@ -17,6 +18,16 @@ namespace Bulky.DataAccess.Data
                 new Category { Id = 2, Name = "Si-Fi", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "Romance", DisplayOrder = 3 }
                 );
+
+            modelBuilder.Entity<Product>().HasData(
+               new Product { Id = 1, Title = "Spy x Family",
+                   Author = "someone", Description = "A family with their own secrets and mission unknown to each other.",
+                   ISBN = "IS55",
+                   ListPrice = 555,
+                   Price = 555,
+                   Price50 = 540,
+                   Price100 = 500}
+               );
         }
     }
 }
---
+++ Bulky.DataAccess/Migrations/20231226064156_AddProductTabletoDb.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductTabletoDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    ISBN = table.Column<string>(type: "text", nullable: false),
+                    Author = table.Column<string>(type: "text", nullable: false),
+                    ListPrice = table.Column<double>(type: "double precision", nullable: false),
+                    Price = table.Column<double>(type: "double precision", nullable: false),
+                    Price50 = table.Column<double>(type: "double precision", nullable: false),
+                    Price100 = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Author", "Description", "ISBN", "ListPrice", "Price", "Price100", "Price50", "Title" },
+                values: new object[] { 1, "someone", "A family with their own secrets and mission unknown to each other.", "IS55", 555.0, 555.0, 500.0, 540.0, "Spy x Family" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+        }
+    }
+}
